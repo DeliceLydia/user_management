1c48321577187fa062e3a7755101a27a8d847a2d
delicelydia <Delydia84@gmail.com>
2022-04-11 17:57:33 +0200

    Add update API

--- controllers/index.js
+++ controllers/index.js
@@ -1,16 +1,34 @@
-const models = require('../database/models');
+const models = require("../database/models");
 
-const createUser = async (req, res) => {
-  try {
-    const user = await models.User.create(req.body);
-    return res.status(201).json({
-      user,
-    });
-  } catch (error) {
-    return res.status(500).json({error: error.message})
+class User {
+  static async createUser(req, res) {
+    try {
+      const user = await models.User.create(req.body);
+      return res.status(201).json({
+        user,
+      });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+  }
+
+  static async updateUser(req, res) {
+    try {
+      const { userId } = req.params;
+      const [updated] = await models.User.update(req.body, {
+        where: { id: userId },
+      });
+      if (updated) {
+        const updatedUser = await models.User.findOne({
+          where: { id: userId },
+        });
+        return res.status(200).json({ user: updatedUser });
+      }
+      throw new Error("User not found");
+    } catch (error) {
+      return res.status(500).send(error.message);
+    }
   }
 }
 
-module.exports = {
-  createUser,
-}
+module.exports = User;
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,6 @@ const router = Router();
 
 router.get('/', (req, res) => res.send('Welcome'))
 router.post('/users', controllers.createUser);
+router.put('/users/:userId', controllers.updateUser);
 
 module.exports = router;
