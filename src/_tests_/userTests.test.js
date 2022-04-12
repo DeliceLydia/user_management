8027c1a4cb4c21357eab7567cd14946638dc6e2d
delicelydia <Delydia84@gmail.com>
2022-04-12 14:48:09 +0200

    Add endpoints tests

--- src/_tests_/userTests.test.js
+++ src/_tests_/userTests.test.js
@@ -13,4 +13,24 @@ describe('Post user endpoint', () => {
       });
     expect(res.statusCode).toEqual(201);
   });
+
+  it('should update a user', async () => {
+    const res = await request(app)
+      .put('/api/users/1')
+      .send({
+        userId: 1,
+        name: 'lydie',
+        email: 'delydia@gmail.com',
+        role: 'developer'
+      });
+
+    expect(res.statusCode).toEqual(200);
+  })
+
+  it('should delet a user', async () => {
+    const res = await request(app)
+      .delete('/api/users/1')
+    expect(res.statusCode).toEqual(204);
+  })
+    
 });
